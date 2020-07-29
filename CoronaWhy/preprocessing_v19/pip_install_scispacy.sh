1f6c2d73f4b789bcf4c78b56e24e8f4897f467e3
ant <polishka@cs.ucr.edu>
2020-07-29 14:11:40 -0700

    add scripts to download/install scispacy models

---
+++ CoronaWhy/preprocessing_v19/pip_install_scispacy.sh
@@ -0,0 +1,5 @@
+pip install ./en_core_sci_lg-0.2.5.tar.gz
+pip install ./en_ner_craft_md-0.2.5.tar.gz
+pip install ./en_ner_jnlpba_md-0.2.5.tar.gz
+pip install ./en_ner_bc5cdr_md-0.2.5.tar.gz
+pip install ./en_ner_bionlp13cg_md-0.2.5.tar.gz
---
+++ CoronaWhy/preprocessing_v19/scispacy_download.sh
@@ -0,0 +1,7 @@
+#wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_core_sci_sm-0.2.5.tar.gz ./scispacy/. &
+#wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_core_sci_md-0.2.5.tar.gz ./scispacy/. &
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_core_sci_lg-0.2.5.tar.gz ./scispacy/. &
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_ner_craft_md-0.2.5.tar.gz ./scispacy/. &
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_ner_jnlpba_md-0.2.5.tar.gz ./scispacy/. &
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_ner_bc5cdr_md-0.2.5.tar.gz ./scispacy/. &
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_ner_bionlp13cg_md-0.2.5.tar.gz ./scispacy/. &
